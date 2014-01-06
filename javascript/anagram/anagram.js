ca2b40e51402aed0b002a22f0b86c50e16530964
marclitchfield <marclitchfield@gmail.com>
2014-01-05 20:57:51 -0800

    anagram iteration 1

---
+++ javascript/anagram/anagram.js
@@ -0,0 +1,21 @@
+var Anagram = function(word) {
+  var sortLetters = function(word) {
+    return word.toLowerCase().split('').sort().join('');
+  };
+
+  var sortedLetters = sortLetters(word);
+
+  var isAnagram = function(testWord) {
+    return testWord !== word && sortLetters(testWord) === sortedWord;
+  };
+
+  var matchTestWords = function(testWords) {
+    return testWords.filter(isAnagram);
+  };
+
+  return {
+    match: matchTestWords
+  };
+};
+
+module.exports = Anagram;
