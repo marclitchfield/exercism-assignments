59b4fb1228096cd01eb2cd1d34e893a07625cc29
marclitchfield <marclitchfield@gmail.com>
2014-01-05 18:14:23 -0800

    word-count iteration 1

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,17 @@
+var splitIntoLowercaseWords = function(text) {
+  var lowercaseWords = text.toLowerCase().split(/\W+/);
+  return lowercaseWords.filter(function whereExists(word) { 
+    return word !== '';
+  });
+}
+
+var wordCounts = function(text) {
+  var counts = {};
+  var words = splitIntoLowercaseWords(text);
+  words.forEach(function incrementCountForWord(word) { 
+    counts[word] = (counts[word] || 0) + 1; 
+  });
+  return counts;
+}
+
+module.exports = wordCounts;
