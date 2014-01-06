b9f41871f0ce7bab3e265e8dd51e96c49f21e8d1
marclitchfield <marclitchfield@gmail.com>
2014-01-05 20:26:43 -0800

    Iteration 4: move Message and Tone out of Bob class and to the top level. They can be used indepdently from Bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,22 +1,22 @@
+var Message = function(text) {
+  return {
+    containsALetter: function() { return text.toLowerCase() !== text.toUpperCase(); },
+    isAllCaps: function() { return text === text.toUpperCase(); },
+    endsWith: function(character) { return text.charAt(text.length - 1) === character; },
+    isAllWhitespace: function() { return text.trim() === ''; }
+  };
+};
+
+var Tone = function(text) {
+  var message = new Message(text);
+  return {
+    isShouting: function() { return message.containsALetter() && message.isAllCaps(); },
+    isQuestion: function() { return message.endsWith('?'); },
+    isSilence: function() { return message.isAllWhitespace(); }
+  };
+};
+
 var Bob = function() {
-  function Message(text) {
-    return {
-      containsALetter: function() { return text.toLowerCase() !== text.toUpperCase(); },
-      isAllCaps: function() { return text === text.toUpperCase(); },
-      endsWith: function(character) { return text.charAt(text.length - 1) === character; },
-      isAllWhitespace: function() { return text.trim() === ''; }
-    };
-  }
-
-  function Tone(text) {
-    var message = new Message(text);
-    return {
-      isShouting: function() { return message.containsALetter() && message.isAllCaps(); },
-      isQuestion: function() { return message.endsWith('?'); },
-      isSilence: function() { return message.isAllWhitespace(); }
-    };
-  }
-
   return {
     hey: function(inputText) {
       var tone = new Tone(inputText);
