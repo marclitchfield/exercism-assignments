7d4b369682a960b9c3cd4f786fddb61700b1e77e
marclitchfield <marclitchfield@gmail.com>
2014-01-06 19:44:17 -0800

    Rename methods to separate commands from queries. Use match instead of split, which eliminates the need for filter.

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,13 +1,10 @@
-var splitIntoLowercaseWords = function(text) {
-  var lowercaseWords = text.toLowerCase().split(/\W+/);
-  return lowercaseWords.filter(function whereExists(word) { 
-    return word !== '';
-  });
+var lowercaseWords = function(text) {
+  return text.toLowerCase().match(/\w+/g);
 };
 
 var wordCounts = function(text) {
   var counts = {};
-  var words = splitIntoLowercaseWords(text);
+  var words = lowercaseWords(text);
   words.forEach(function incrementCountForWord(word) { 
     counts[word] = (counts[word] || 0) + 1; 
   });
