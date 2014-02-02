8499937180fe30565fc9f8760030444cde43285e
marclitchfield <marclitchfield@gmail.com>
2014-02-01 16:50:38 -0800

    Updated beer song to use regexes

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,37 +1,30 @@
 function verse(number) {
+  return capitalize(transform("$X bottle(s) of beer on the wall, $X bottle(s) of beer.\n" +
+    "[Take $some down and pass it around|Go to the store and buy some more], " +
+    "[$X-1|99] bottle(-1:s) of beer on the wall.\n"));
 
-  function bottlesRemaining(nextNumber) {
-    var remaining = nextNumber !== undefined ? nextNumber : number;
-    if (remaining === 0) { return 'no more bottles of beer'; }
-    if (remaining === 1) { return '1 bottle of beer'; }
-    return remaining + ' bottles of beer';
-  }
-
-  function next() {
-    return number === 0 ? 99 : number - 1;
-  }
-
-  function pronoun() {
-    return number === 1 ? 'it' : 'one';
-  }
-
-  function action() {
-    if (number > 0) { return 'Take ' + pronoun() + ' down and pass it around'; }
-    return 'Go to the store and buy some more';
+  function transform(pattern) {
+    return pattern
+      .replace(/\$X-1/g, numberText(number-1))
+      .replace(/\$X/g, numberText(number))
+      .replace(/\$some/g, number === 1 ? 'it' : 'one')
+      .replace(/\(s\)/g, number === 1 ? '' : 's')
+      .replace(/\(-1:s\)/g, number-1 === 1 ? '' : 's')
+      .replace(/\[(.*?)\|(.*?)\]/g, number > 0 ? '$1' : '$2');
   }
 
   function capitalize(text) {
     return text.charAt(0).toUpperCase() + text.slice(1);
   }
 
-  return capitalize(bottlesRemaining() + ' on the wall, ' 
-          + bottlesRemaining() + '.\n' + action() + ', '
-          + bottlesRemaining(next()) + ' on the wall.\n');
+  function numberText(number) {
+    return number === 0 ? 'no more' : number;
+  }
 }
 
 function sing(fromNumber, toNumber) {
   if (fromNumber > (toNumber || 0)) {
-    return verse(fromNumber) + '\n' + sing(fromNumber - 1, toNumber);
+    return verse(fromNumber) + '\n' + sing(fromNumber-1, toNumber);
   }
   return verse(fromNumber);
 }
